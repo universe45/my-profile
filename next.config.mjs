df324621d2ed67b6a0b28e27b57bb0978b0c8d5c
TITIPONG WANNACHAI <ttp.wannachai@gmail.com>
2025-03-06 17:47:10 +0700

    System: Update package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/my-profile',
+    generateBuildId: async () => {
+        return `${Date.now()}`; // Unique build ID to prevent stale cache
+      },
+      experimental: {
+        incrementalCacheHandlerPath: false, // Disables cache persistence
+      },
+      outputFileTracing: false, // Avoids caching dependencies
+      images: {
+        unoptimized: true, // Ensures images are not cached
+      },
+      async headers() {
+        return [
+          {
+            source: "/(.*)",
+            headers: [
+              { key: "Cache-Control", value: "no-store, no-cache, must-revalidate, proxy-revalidate" },
+              { key: "Pragma", value: "no-cache" },
+              { key: "Expires", value: "0" },
+            ],
+          },
+        ];
+      },
 };
-
 export default nextConfig;
