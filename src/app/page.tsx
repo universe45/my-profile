ace2ad9dad7bcd9131f69d8b9681c05cdf8fa9d1
UNXVERSE <ttp.wannachai@gmail.com>
2025-03-17 13:37:14 +0700

    Improve seurity check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,14 @@
 "use client";
-import BottomNav from "@/components/bottomNav";
-import InfoCard from "@/components/infoCard";
-import Card from "@/components/card";
 import MyCard from "@/components/myCard";
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
-  const [devToolsOpen, setDevToolsOpen] = useState(true);
+  const [devToolsOpen, setDevToolsOpen] = useState(false);
 
   const checkMobile = () => {
     setIsMobile(window.innerWidth <= 667 || window.innerHeight <= 375);
   };
-
-  const detectDevTools = React.useCallback(() => {
-    if (!isMobile) {
-      const threshold = 160;
-      const widthThreshold = window.outerWidth - window.innerWidth > threshold;
-      const heightThreshold = window.outerHeight - window.innerHeight > threshold;
-      setDevToolsOpen(widthThreshold || heightThreshold);
-    } else {
-      setDevToolsOpen(false);
-    }
-  }, [isMobile]);
 
   const disableRightClick = (event: MouseEvent) => {
     event.preventDefault();
@@ -34,37 +20,23 @@ export default function Home() {
 
     if (keysToDisable.includes(event.key) && (event.key === "F12" || isCtrlShift)) {
       event.preventDefault();
-    }
-  }
-
-  const detectDevToolsAdvanced = () => {
-    const devtools = /./;
-    devtools.toString = () => {
       setDevToolsOpen(true);
-      return '';
-    };
-    console.log(devtools);
+    }
   };
 
   useEffect(() => {
     checkMobile();
-    detectDevTools();
-    detectDevToolsAdvanced();
 
     window.addEventListener("resize", checkMobile);
-    window.addEventListener("resize", detectDevTools);
-    window.addEventListener("devtoolschange", detectDevTools);
     window.addEventListener("contextmenu", disableRightClick);
     window.addEventListener("keydown", disableKeyDown);
 
     return () => {
       window.removeEventListener("resize", checkMobile);
-      window.removeEventListener("resize", detectDevTools);
-      window.removeEventListener("devtoolschange", detectDevTools);
       window.removeEventListener("contextmenu", disableRightClick);
       window.removeEventListener("keydown", disableKeyDown);
     };
-  }, [detectDevTools, isMobile]);
+  }, []);
 
   const devToolsBypass = process.env.NEXT_PUBLIC_DEVTOOLS_BYPASS === "true";
 
